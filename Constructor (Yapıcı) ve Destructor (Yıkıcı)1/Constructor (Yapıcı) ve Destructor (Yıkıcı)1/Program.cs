882960c7050c23f80116e1c984dccdbda9731e68
Mithat <mithatsirmali@gmail.com>
2025-05-22 16:25:45 +0300

    Add files via upload

---
+++ Constructor (Yapıcı) ve Destructor (Yıkıcı)1/Constructor (Yapıcı) ve Destructor (Yıkıcı)1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor__Yapıcı__ve_Destructor__Yıkıcı_1
+{
+    internal class Program
+    {
+        class Kitap
+        {
+            public string Ad;
+            public string Yazar;
+
+            // Varsayılan constructor
+            public Kitap()
+            {
+                Ad = "Bilinmiyor";
+                Yazar = "Bilinmiyor";
+                Console.WriteLine("Varsayılan yapıcı çalıştı.");
+            }
+
+            // Parametreli constructor
+            public Kitap(string ad, string yazar)
+            {
+                Ad = ad;
+                Yazar = yazar;
+                Console.WriteLine("Parametreli yapıcı çalıştı.");
+            }
+
+            // Destructor
+            ~Kitap()
+            {
+                Console.WriteLine("Kitap nesnesi bellekten siliniyor...");
+            }
+
+            public void Yazdir()
+            {
+                Console.WriteLine($"Kitap Adı: {Ad}, Yazar: {Yazar}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Kitap k1 = new Kitap(); // Varsayılan constructor
+            k1.Yazdir();
+
+            Kitap k2 = new Kitap("1984", "George Orwell"); // Parametreli constructor
+            k2.Yazdir();
+
+            // Destructor, garbage collector tarafından otomatik çağrılır.
+            // Elle çağrılamaz ancak uygulama kapanırken görülebilir.
+        }
+    }
+}
